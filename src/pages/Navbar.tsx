534aa0e49caf169faec61907531f3fd06d9b525f
JayDampitan <jaykennethpd@gmail.com>
2023-03-09 13:28:49 -0800

    install framer motion and apply animation when toggled on mobile navbar.

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -6,6 +6,25 @@ import { CgMenuRight, CgClose } from "react-icons/cg";
 import { SlSocialInstagram, SlSocialFacebook } from "react-icons/sl";
 import { NAV__LINKS } from "@/assets/data/navData";
 import Link from "next/link";
+import { motion, AnimatePresence } from "framer-motion";
+
+const mobileVariant = {
+  hidden: {
+    y: "-100vh"
+  },
+  visible: {
+    y: 0,
+    transition: {
+      duration: 1
+    }
+  },
+  exit: {
+    y:"-100vh",
+    transition: {
+      duration: 1
+    }
+  }
+}
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
@@ -40,43 +59,48 @@ const Navbar = () => {
         <CgMenuRight />
       </div>
       {/*-------------Open Mobile Menu--------------*/}
-      <div
-        className={`${
-          navbarOpen ? "block" : "hidden"
-        } fixed sm:hidden inset-0 bg-secondary h-screen text-off-white`}
-      >
-        <section className="flex justify-around  items-center mb-28">
-          <div className="h-14 w-32 rounded flex items-center my-8">
-            <Link href={"/"} onClick={handleNavbar}>
-              <Image
-                src={WhiteLogo}
-                alt="good things logo"
-                className="h-full w-full"
-              />
-            </Link>
-          </div>
-          <div onClick={handleNavbar} className="text-4xl">
-            <CgClose />
-          </div>
-        </section>
-        <section className="h-3/6 text-3xl text-center flex-col justify-center items-center">
-          <ul>
-            {NAV__LINKS.map((item, index) => (
-              <li
-                onClick={handleNavbar}
-                className="px-8 text-off-white my-10"
-                key={index}
-              >
-                <Link href={`${item.path}`}>{item.display}</Link>
-              </li>
-            ))}
-          </ul>
-          <section className="flex text-3xl justify-center py-5">
-            <SlSocialInstagram className="mx-4" />
-            <SlSocialFacebook className="mx-4" />
-          </section>
-        </section>
-      </div>
+      <AnimatePresence>
+        {navbarOpen && (
+          <motion.div
+          variants={mobileVariant}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          className="block fixed sm:hidden inset-0 bg-secondary h-screen text-off-white">
+            <section className="flex justify-around  items-center mb-28">
+              <div className="h-14 w-32 rounded flex items-center my-8">
+                <Link href={"/"} onClick={handleNavbar}>
+                  <Image
+                    src={WhiteLogo}
+                    alt="good things logo"
+                    className="h-full w-full"
+                  />
+                </Link>
+              </div>
+              <div onClick={handleNavbar} className="text-4xl">
+                <CgClose />
+              </div>
+            </section>
+            <section className="h-3/6 text-3xl text-center flex-col justify-center items-center">
+              <ul>
+                {NAV__LINKS.map((item, index) => (
+                  <li
+                    onClick={handleNavbar}
+                    className="px-8 text-off-white my-10"
+                    key={index}
+                  >
+                    <Link href={`${item.path}`}>{item.display}</Link>
+                  </li>
+                ))}
+              </ul>
+              <section className="flex text-3xl justify-center py-5">
+                <SlSocialInstagram className="mx-4" />
+                <SlSocialFacebook className="mx-4" />
+              </section>
+            </section>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </main>
   );
 };
